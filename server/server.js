0e41bfd31e0a71f6c976c085c326c84c5d92c152
Mike Boyd <mboyd3083@gmail.com>
2023-05-12 13:44:49 -0400

    initial backend setup

---
+++ server/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import connectDB from "./db/connectDB.js";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+app.get("/", (req, res) => {
+  res.send("hello there");
+});
+
+const PORT = process.env.PORT || 9000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
