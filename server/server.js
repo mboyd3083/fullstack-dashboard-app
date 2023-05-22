91be9344c65e2de164a13f56d2b05c29141874de
Mike Boyd <mboyd3083@gmail.com>
2023-05-22 13:50:22 -0400

    modified server

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+//data imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
@@ -38,6 +42,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    // User.insertMany(dataUser);
   } catch (error) {
     console.log(error);
   }
