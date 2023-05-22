850579fa2c99e7c949242ea264f7f6d0a1bb9fdf
Mike Boyd <mboyd3083@gmail.com>
2023-05-22 13:45:26 -0400

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
           <CssBaseline />
           <Routes>
             <Route element = {<Layout/>}>
-              <Route path ="/" element= {<Navigate to = "/dashboard" replace/>}/>
+              <Route path ="/" element= {<Navigate to = "/dashboard" replace />}/>
               <Route path ="/dashboard" element= {<Dashboard/>}/>
             </Route>
           </Routes>
