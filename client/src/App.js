f73362f0a007c741040ff7174b002aa2f73436f3
Mike Boyd <mboyd3083@gmail.com>
2023-05-25 14:19:50 -0400

    added customer and product routes on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,13 @@ import { createTheme } from "@mui/material/styles";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from 'scenes/dashboard';
 import Layout from 'scenes/layout';
-
+import Products from "scenes/products"
+import Customers from "scenes/customers"
  
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
+ 
   return (
     <div className="app">
       <BrowserRouter>
@@ -21,6 +22,8 @@ const App = () => {
             <Route element = {<Layout/>}>
               <Route path ="/" element= {<Navigate to = "/dashboard" replace />}/>
               <Route path ="/dashboard" element= {<Dashboard/>}/>
+              <Route path ="/products" element= {<Products/>}/>
+              <Route path ="/customers" element= {<Customers/>}/>
             </Route>
           </Routes>
         </ThemeProvider>
