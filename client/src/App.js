cbd9d122298afe4453f74f05e1cf687c0459d60e
Mike Boyd <mboyd3083@gmail.com>
2023-06-01 13:16:06 -0400

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,13 @@ import Products from "scenes/products"
 import Customers from "scenes/customers"
 import Transactions from "scenes/transactions"
 import Geography from "scenes/geography"
+import Overview from "scenes/overview"
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
+import Admin from "scenes/admin";
+import Performance from "scenes/performance";
+
  
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -21,13 +28,19 @@ const App = () => {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route element = {<Layout/>}>
-              <Route path ="/" element= {<Navigate to = "/dashboard" replace />}/>
-              <Route path ="/dashboard" element= {<Dashboard/>}/>
-              <Route path ="/products" element= {<Products/>}/>
-              <Route path ="/customers" element= {<Customers/>}/>
-              <Route path ="/transactions" element= {<Transactions/>}/>
-              <Route path ="/geography" element= {<Geography/>}/>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/performance" element={<Performance />} />
             </Route>
           </Routes>
         </ThemeProvider>
